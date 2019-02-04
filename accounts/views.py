84aaeb6410f455113faac89d0a2261387465c784
Cesar Bernal <cesarbworkmail@gmail.com>
2019-02-04 15:46:35 +0000

    login logout setup

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,reverse
+from django.contrib import auth, messages
 
 # Create your views here.
 
 def index(request):
     ''' returning index.html '''
     return render(request, 'index.html')
+    
+def logout(request):
+    """ log user out """
+    auth.logout(request)
+    messages.success(request,"You have been logged out")
+    return redirect(reverse('index'))
+    
+def login(request):
+    ''' return login '''
+    return render(request,'login.html')
     
